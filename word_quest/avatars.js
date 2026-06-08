e048b2c5d7914a544529a5cdbf3a75492840301b
Wanderdark <oktavfirat@gmail.com>
2026-06-08 14:09:15 +0300

    word quest early access new version update

--- word_quest/avatars.js
+++ word_quest/avatars.js
@@ -20,15 +20,15 @@
 // ============================================================
 
 const AVATARS = [
-  {src:'avatars/AV_Anime_Blaze.webp',  facesRight:false},
-  {src:'avatars/AV_Anime_Frost.webp',  facesRight:false},
-  {src:'avatars/AV_Anime_Grace.webp',  facesRight:false},
-  {src:'avatars/AV_Anime_Louis.webp',  facesRight:false},
-  {src:'avatars/AV_Anime_Mara.webp',   facesRight:false},
-  {src:'avatars/AV_Anime_Nova.webp',   facesRight:true},
-  {src:'avatars/AV_Anime_Orion.webp',  facesRight:false},
-  {src:'avatars/AV_Anime_Pixie.webp',  facesRight:false},
-  {src:'avatars/AV_Anime_Rex.webp',    facesRight:false},
-  {src:'avatars/AV_Anime_Terra.webp',  facesRight:true},
+  {src:'avatars/AV_Anime_Blaze.webp', dmgSrc:'avatars/AV_Anime_Blaze_dmg.webp', facesRight:false},
+  {src:'avatars/AV_Anime_Frost.webp', dmgSrc:'avatars/AV_Anime_Frost_dmg.webp', facesRight:false},
+  {src:'avatars/AV_Anime_Grace.webp', dmgSrc:'avatars/AV_Anime_Grace_dmg.webp', facesRight:false},
+  {src:'avatars/AV_Anime_Louis.webp', dmgSrc:'avatars/AV_Anime_Louis_dmg.webp', facesRight:false},
+  {src:'avatars/AV_Anime_Mara.webp',  dmgSrc:'avatars/AV_Anime_Mara_dmg.webp',  facesRight:false},
+  {src:'avatars/AV_Anime_Nova.webp',  dmgSrc:'avatars/AV_Anime_Nova_dmg.webp',  facesRight:true},
+  {src:'avatars/AV_Anime_Orion.webp', dmgSrc:'avatars/AV_Anime_Orion_dmg.webp', facesRight:false},
+  {src:'avatars/AV_Anime_Pixie.webp', dmgSrc:'avatars/AV_Anime_Pixie_dmg.webp', facesRight:false},
+  {src:'avatars/AV_Anime_Rex.webp',   dmgSrc:'avatars/AV_Anime_Rex_dmg.webp',   facesRight:false},
+  {src:'avatars/AV_Anime_Terra.webp', dmgSrc:'avatars/AV_Anime_Terra_dmg.webp', facesRight:true},
   // buraya ekle ↓
 ];
